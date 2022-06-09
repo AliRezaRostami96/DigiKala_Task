4bf0fb5a7a546221cfac91f505f06d4606b1be10
AliReza Rostami <aliirezaa96@gmail.com>
2022-06-10 02:02:12 +0430

    update cartModal

--- src/components/Public/cartModal/index.tsx
+++ src/components/Public/cartModal/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { Button, Modal } from 'react-bootstrap';
+import { Alert, Button, Modal } from 'react-bootstrap';
+import { useSelector } from 'react-redux';
+import { ReducerModel } from '../../../store/reducers/cartReducer';
+import { ProductDetailsModel } from '../../productDetails/setting';
+import { ProductModel } from '../../products/setting';
+import ProductComponent from './components/product';
 import { Translation } from './setting';
 
 interface props {
@@ -8,12 +13,17 @@ interface props {
 }
 
 const CartModal: React.FC<props> = (props: props) => {
+    const cartList: Array<ProductDetailsModel | ProductModel> = useSelector((state: ReducerModel) => state.cartList);
+    console.log("CartModal");
+    console.log(cartList);
+
     return (
         <Modal
             {...props}
             size="lg"
             aria-labelledby="contained-modal-title-vcenter"
             centered
+            scrollable={true}
         >
             <Modal.Header closeButton>
                 <Modal.Title id="contained-modal-title-vcenter">
@@ -21,12 +31,16 @@ const CartModal: React.FC<props> = (props: props) => {
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
-                <h4>Centered Modal</h4>
-                <p>
-                    Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
-                    dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
-                    consectetur ac, vestibulum at eros.
-                </p>
+                {
+                    cartList.map(item => (
+                        <ProductComponent product={item} />
+                    ))
+                }
+                {
+                    cartList.length == 0 && <Alert variant="light">
+                        {Translation.noItem}
+                    </Alert>
+                }
             </Modal.Body>
             <Modal.Footer>
                 <Button onClick={props.onHide}>{Translation.close}</Button>
--- src/components/Public/cartModal/setting.tsx
+++ src/components/Public/cartModal/setting.tsx
@@ -1,5 +1,8 @@
 export const Translation = {
     cart: "سبد خرید",
     close: "بستن",
-    
+    noItem: "کالایی در سبد شما ثبت نشده است!",
+    rate: "امتیاز",
+    price: "قیمت",
+    delete: "حذف"
 }
