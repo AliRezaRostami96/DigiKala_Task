6879bde00a4a3b1a3dd6deaa797dfd4c31614796
AliReza Rostami <aliirezaa96@gmail.com>
2022-06-10 23:51:42 +0430

    finish filter component

--- src/components/products/components/filter.tsx
+++ src/components/products/components/filter.tsx
@@ -1,10 +1,104 @@
-import React from 'react';
+import { Button, FormControl, InputLabel, MenuItem, Select, Slider, TextField } from '@mui/material';
+import React, { useState } from 'react';
+import { ProductQueryModel } from '../../../hooks/useProductQuery';
+import { Translation } from '../setting';
 
-const FilterComponent: React.FC = () => {
+interface props {
+    setQueries: (v: ProductQueryModel) => VoidFunction
+}
 
+const initialValues: ProductQueryModel = {
+    'price[min]': 50,
+    'price[max]': 1000000,
+    q: "",
+    sort: 4
+}
+
+const FilterComponent: React.FC<props> = ({ setQueries }: props) => {
+
+    const [query, setQuery] = useState<string>(initialValues.q);
+    const [priceRange, setPriceRange] = useState<number[]>([initialValues['price[min]'], initialValues['price[max]']]);
+    const [sort, setSort] = useState<number>(initialValues.sort);
+
+    const valueLabelFormat = (value: number): string => {
+        return (value).toFixed(1).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    }
+
+    const minDistance = 10;
+    const onPriceChange = (
+        event: Event,
+        newValue: number | number[],
+        activeThumb: number,
+    ): void => {
+        if (!Array.isArray(newValue)) {
+            return;
+        }
+
+        if (activeThumb === 0) {
+            setPriceRange([Math.min(newValue[0], priceRange[1] - minDistance), priceRange[1]]);
+        } else {
+            setPriceRange([priceRange[0], Math.max(newValue[1], priceRange[0] + minDistance)]);
+        }
+    };
+
+    const getQueries = (): ProductQueryModel => {
+        const priceMin = priceRange[0];
+        const priceMax = priceRange[1];
+        const queries: ProductQueryModel = {
+            "price[min]": priceMin,
+            "price[max]": priceMax,
+            q: query,
+            sort: sort
+        }
+        return queries;
+    }
+
+    const onFilter = (): void => {
+        console.log("getQueries()");
+        console.log(getQueries());
+        
+        setQueries(getQueries());
+
+    }
 
     return (
-        <></>
+        <>
+            <TextField
+                id="search"
+                label={Translation.search}
+                variant="outlined"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
+
+            <FormControl fullWidth>
+                <InputLabel id="sort-label">{Translation.sort}</InputLabel>
+                <Select
+                    labelId="sort-label"
+                    id="sort-field"
+                    label={Translation.sort}
+                    value={sort}
+                    onChange={(e) => setSort(+e.target.value)}
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+
+            <Slider
+                getAriaLabel={() => 'Temperature range'}
+                value={priceRange}
+                onChange={onPriceChange}
+                min={initialValues['price[min]']}
+                step={1}
+                max={initialValues['price[max]']}
+                valueLabelDisplay="on"
+                valueLabelFormat={valueLabelFormat}
+            />
+
+            <Button variant="contained" onClick={onFilter}>{Translation.filter}</Button>
+        </>
     )
 }
 
