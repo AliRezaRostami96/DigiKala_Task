002caf045360436332df7e4fd680378a5a6222a1
AliReza Rostami <aliirezaa96@gmail.com>
2022-06-10 13:48:28 +0430

    add material rtl

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
+import { ThemeProvider } from '@mui/material';
 import React from 'react';
 import { Provider } from 'react-redux';
 import './App.css';
 import LayoutComponent from './components/Public/layout';
 import Routing from './pages/routing';
 import store from './store/store';
+import RTL from './theme/rtl';
+import theme from './theme/theme';
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <LayoutComponent>
-          <Routing></Routing>
-        </LayoutComponent>
+        <RTL>
+          <ThemeProvider theme={theme}>
+            <LayoutComponent>
+              <Routing></Routing>
+            </LayoutComponent>
+          </ThemeProvider>
+        </RTL>
       </Provider>
     </div>
   );
---
+++ src/theme/rtl.tsx
@@ -0,0 +1,22 @@
+
+
+import rtlPlugin from 'stylis-plugin-rtl';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import { prefixer } from 'stylis';
+
+interface props {
+    children: React.ReactNode
+}
+
+// Create rtl cache
+const cacheRtl = createCache({
+    key: 'muirtl',
+    stylisPlugins: [prefixer, rtlPlugin],
+});
+
+const RTL = ({ children }: props) => {
+    return <CacheProvider value={cacheRtl}>{children}</CacheProvider>;
+}
+
+export default RTL;
