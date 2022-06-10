cafc5a707d2d2ed2e1f450eb409d14255c8b815c
AliReza Rostami <aliirezaa96@gmail.com>
2022-06-10 23:50:09 +0430

    remove page from product query

--- src/hooks/useProductQuery.tsx
+++ src/hooks/useProductQuery.tsx
@@ -2,8 +2,14 @@ import React, { useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 
 export interface ProductQueryModel {
-    page?: string,
-    sort?: string,
+    sort: number,
+    'price[min]': number,
+    'price[max]': number,
+    q: string
+}
+
+interface StringProductQueryModel {
+    sort: string,
     'price[min]': string,
     'price[max]': string,
     q: string
@@ -12,16 +18,28 @@ export interface ProductQueryModel {
 const UseProductQuery = (): [ProductQueryModel, any] => {
     const [searchParams, setSearchParams] = useSearchParams();
 
-    const [queries, setQueries] = useState<ProductQueryModel>({
-        page: searchParams.get("page") ?? "1",
-        sort: searchParams.get("sort") ?? "4",
-        "price[min]": searchParams.get("price[min]") ?? '1',
-        "price[max]": searchParams.get("price[max]") ?? '99999999',
+    const initialValue = {
+        page: Number(searchParams.get("page")) ?? "1",
+        sort: Number(searchParams.get("sort")) ?? "4",
+        "price[min]": Number(searchParams.get("price[min]")) ?? 1,
+        "price[max]": Number(searchParams.get("price[max]")) ?? 99999999,
         q: searchParams.get("q") ?? "",
-    });
+    }
+
+    const [queries, setQueries] = useState<ProductQueryModel>(initialValue);
+
+    const getStringQueries = (q: ProductQueryModel): StringProductQueryModel => {
+        const newQuery: StringProductQueryModel = {
+            "price[max]": q['price[max]'].toString(),
+            "price[min]": q['price[min]'].toString(),
+            q: q.q,
+            sort: q.sort.toString(),
+        }
+        return newQuery;
+    }
 
     const updateQuery = (query: ProductQueryModel): any => {
-        setSearchParams({ ...query });
+        setSearchParams({ ...getStringQueries(query) });
         setQueries(query);
     }
 
