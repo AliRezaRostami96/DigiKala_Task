17e80a0a30393712c81cbb374b5c95e19699640e
AliReza Rostami <aliirezaa96@gmail.com>
2022-06-11 12:39:31 +0430

    create image gallery

--- src/components/productDetails/components/productImage.tsx
+++ src/components/productDetails/components/productImage.tsx
@@ -9,11 +9,11 @@ const ProductImageComponent: React.FC<props> = ({ images }: props) => {
     const [mainImage, setMainImage] = useState<string>(images.main.url[0]);
     const [imagesList, setImagesList] = useState(images.list);
     return (
-        <div className='w-max'>
-            <div className='inline-flex justify-center items-center shadow-md h-72 m-0'>
-                <img src={mainImage} width={288} alt="mainImage" />
+        <>
+            <div className='inline-flex justify-center items-center shadow-md m-0 p-4'>
+                <img src={mainImage} alt="mainImage" />
             </div>
-            <div className='w-full flex justify-center mt-4'>
+            <div className='w-full flex justify-center mt-4 overflow-auto'>
                 {
                     imagesList.map((img, index) => (
                         <img
@@ -21,13 +21,13 @@ const ProductImageComponent: React.FC<props> = ({ images }: props) => {
                             width={100}
                             key={`image_${index}`}
                             alt="image"
-                            className='mx-2'
+                            className='mx-2 cursor-pointer'
                             onClick={() => setMainImage(img.url[0])}
                         />
                     ))
                 }
             </div>
-        </div>
+        </>
     )
 
 }
