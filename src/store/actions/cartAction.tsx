7d42a089103d423d5939c5740434429b5fbc27ca
AliReza Rostami <aliirezaa96@gmail.com>
2022-06-11 12:40:50 +0430

    add redux thunk to store

--- src/store/actions/cartAction.tsx
+++ src/store/actions/cartAction.tsx
@@ -1,16 +1,37 @@
-import { ProductDetailsModel } from "../../components/productDetails/setting"
-import { ProductModel } from "../../components/products/setting"
+import { AnyAction, Dispatch } from "redux";
+import { ThunkAction, ThunkDispatch } from "redux-thunk";
+import { ProductDetailsModel } from "../../components/productDetails/setting";
+import { ProductModel } from "../../components/products/setting";
+import { Translation } from "../../components/Public/layout/setting";
+import { RootState } from "../store";
+import { HideSnackBar, ShowSnackBar } from "./snackbarAction";
 
-export const AddToCart = (Product: ProductDetailsModel | ProductModel) => {
+const AddToCartAction = (Product: ProductDetailsModel | ProductModel) => {
     return {
         type: "ADD_PRODUCT",
         payload: Product
     }
 }
 
-export const RemoveFromCart = (Product: ProductDetailsModel | ProductModel) => {
+const RemoveFromCartAction = (Product: ProductDetailsModel | ProductModel) => {
     return {
         type: "REMOVE_PRODUCT",
         payload: Product
     }
 }
+
+export const AddToCart =
+    (Product: ProductDetailsModel | ProductModel): any => (
+        async (dispatch: Dispatch<AnyAction>) => {
+            dispatch(AddToCartAction(Product));
+            dispatch(ShowSnackBar(Translation.saveToCart, "success"));
+        }
+    )
+
+export const RemoveFromCart =
+    (Product: ProductDetailsModel | ProductModel): any => (
+        async (dispatch: Dispatch<AnyAction>) => {
+            dispatch(RemoveFromCartAction(Product));
+            dispatch(ShowSnackBar(Translation.removeFromCart, "error"));
+        }
+    )
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,12 +1,15 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import CartReducer from './reducers/cartReducer';
 import SnackBarReducer from './reducers/snackbarReducer';
-// ...
 
 const rootReducer = combineReducers({
     cartList: CartReducer,
     snackbar: SnackBarReducer
 })
 
-const store = createStore(rootReducer);
+export type RootState = ReturnType<typeof rootReducer>;
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 export default store;
