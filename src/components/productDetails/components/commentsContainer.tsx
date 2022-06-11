321d5fa68b49e13647c87f1a855c61e80e637634
AliReza Rostami <aliirezaa96@gmail.com>
2022-06-11 12:39:04 +0430

    update comments

--- src/components/productDetails/components/commentsContainer.tsx
+++ src/components/productDetails/components/commentsContainer.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent, Typography } from '@mui/material';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { CommentContainerProps, Translations } from '../setting';
 
 const CommentContainerComponent: React.FC<CommentContainerProps> = ({ comments }: CommentContainerProps) => {
@@ -9,23 +9,25 @@ const CommentContainerComponent: React.FC<CommentContainerProps> = ({ comments }
             <Typography variant='h5' className='fw-bold'>
                 {Translations.comments}
             </Typography>
-            {comments.map((comment, index) => (
-                <Card sx={{ minWidth: 275 }} key={`comment_${index}`} className="mx-4 my-2">
-                    <CardContent>
-                        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
-                            <span>
-                                {comment.user_name}
-                            </span>
-                            <span className='mr-4'>
-                                {comment.created_at}
-                            </span>
-                        </Typography>
-                        <Typography variant="body2" className='pt-2 pr-4'>
-                            {comment.body}
-                        </Typography>
-                    </CardContent>
-                </Card>
-            ))}
+            {
+                comments.map((comment, index) => (
+                    <Card sx={{ minWidth: 275 }} key={`comment_${index}`} className="mx-4 my-2">
+                        <CardContent>
+                            <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+                                <span>
+                                    {comment.user_name}
+                                </span>
+                                <span className='mr-4'>
+                                    {comment.created_at}
+                                </span>
+                            </Typography>
+                            <Typography variant="body2" className='pt-2 pr-4'>
+                                {comment.body}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                ))
+            }
         </div>
     )
 
